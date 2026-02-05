aac82118c896d0da0149539e226002ebbba958e8
I-Need-To-Learn-Backend <leo.mettam@mallaighigh.org.uk>
2026-02-05 10:42:44 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+
+function play_slope(){
+var win = window.open('about:blank');
+var url = "https://bigfoot9999.github.io/Slope-Game/"; // Replace with your game URL
+var iframe = win.document.createElement('iframe');
+iframe.style.width = "100%";
+iframe.style.height = "100%";
+iframe.style.border = "none";
+iframe.src = url;
+win.document.body.appendChild(iframe);
+}
+
+function play_clash_royale(){
+var win = window.open('about:blank');
+var url = "https://web.cloudmoonapp.com/game/com.supercell.clashroyale/"; // Replace with your game URL
+var iframe = win.document.createElement('iframe');
+iframe.style.width = "100%";
+iframe.style.height = "100%";
+iframe.style.border = "none";
+iframe.src = url;
+win.document.body.appendChild(iframe);
+}
